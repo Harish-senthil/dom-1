9e71c295a7651e78ea81962e506753c1b5b6078d
Harish-senthil <162134443+Harish-senthil@users.noreply.github.com>
2024-03-26 20:24:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function display() {
+    var firstName = document.getElementById("firstName").value;
+        var middleName = document.getElementById("middleName").value;
+        var lastName = document.getElementById("lastName").value;
+        var email = document.getElementById("email").value;
+        var address = document.getElementById("address").value;
+        
+        console.log(firstName+ middleName + lastName);
+        console.log(email);
+        console.log(address);
+}
